6eb6a67d47a5485fc1ebe199ed2cb2af03f3e164
mrvigneshvt <mrvigneshvt@gmail.com>
2025-01-28 11:55:49 +0530

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: 'groupX',
-      script: './server.js',
+      name: "groupX",
+      script: "npm start-build",
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: 'production'
-      }
-    }
-  ]
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
